05e4f4020f620ccade50b403f74bfdc769fa18ab
TarasKachmar2022 <107129371+TarasKachmar2022@users.noreply.github.com>
2024-07-26 00:05:34 +0000

    Deploying to gh-pages from @ TarasKachmar2022/grocery-store@d7af14e3f074c80d7f885e62a7e00fcb811b3a52 🚀

---
+++ index.2520e5d6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.getElementById("btn-up");window.addEventListener("scroll",(function(){document.body.scrollTop>400||document.documentElement.scrollTop>400?t.classList.add("btn-up--active"):t.classList.remove("btn-up--active")})),t.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.2520e5d6.js.map
---
+++ index.c96878f3.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("btn-up");window.addEventListener("scroll",(function(){document.body.scrollTop>400||document.documentElement.scrollTop>400?t.classList.add("btn-up--active"):t.classList.remove("btn-up--active")})),t.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.c96878f3.js.map
